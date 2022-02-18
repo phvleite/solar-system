464857a14424c5a6ecb8dc5156e91fecd5d8a4f5
phvleite <phvleite@gmail.com>
2022-02-18 17:21:31 -0300

    Requisitos 2, 3 e 4 finalizados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <main>
+        <Header />
+        <SolarSystem />
+      </main>
     );
   }
 }
