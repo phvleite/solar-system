1bc48ae5eddb2f2519152d934eae3b6c2ca121a9
phvleite <phvleite@gmail.com>
2022-02-18 15:42:53 -0300

    requisito 01 finalizado

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="box-header-solar-system">
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
