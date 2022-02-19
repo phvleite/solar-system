fbf7b4d96e5219b2f06bae29c2e907bfbb46e7af
phvleite <phvleite@gmail.com>
2022-02-18 22:43:52 -0300

    finalizados os requisitos 7 e 8

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
-    console.log(this.props);
     return (
       <div data-testid="planet-card">
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
