42e4dcd16a61383ebb4c283c1efe56cb2409b33b
phvleite <phvleite@gmail.com>
2022-02-18 23:13:25 -0300

    finalizados requisitos 9 e 10, css será no fim de semana

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
+    const dbMissions = missions.map((mission) => (
+      <MissionCard
+        name={ mission.name }
+        year={ mission.year }
+        country={ mission.country }
+        destination={ mission.destination }
+        key={ mission.name }
+      />));
+
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {dbMissions}
       </div>);
   }
 }
